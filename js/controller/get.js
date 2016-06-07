7449ed19e81059f9cda1268343c42bb82b478da6
Gil Vieira <ogilvieira@gmail.com>
2016-06-07 20:26:33 -0300

    ajustes feed

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,7 +1,14 @@
 app.controller("GetController", ['$scope', 'podcasts', 'feed', '$routeParams', function($scope, podcasts, feed, $routeParams){
   $scope.podcasts = false;
+
+  $scope.getEpisodes = function(urlRss){
+    feed.list(urlRss, function(res){
+      console.log(res);
+    });
+  };
+
   podcasts.get($routeParams.podcastSlug, function(res){
     $scope.podcast = res;
-    console.log(res.name);
+    if(res.rss_link){ $scope.getEpisodes(res.rss_link); }
   });
 }]);
--- js/factory/feed.js
+++ js/factory/feed.js
@@ -1,7 +1,18 @@
 app.factory('feed', function($http){
   return {
-    list: function(callback){
-      $http.get('http://loopmatinal.libsyn.com/rss?'+Date.now()).success(callback);
+    list: function(url, callback){
+      $http({
+        method: 'GET',
+        url: url,
+        timeout: 10000,
+        params: {}
+      }).success(function(data){
+        console.log(data);
+        callback(data);
+      }).error(function(){
+        console.warn("error feed");
+        callback(false);
+      });
     }
   };
 });
