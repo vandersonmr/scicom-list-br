c026cb7b67950c175e38d9d4171b9f428197c8a2
ogilvieira <ogilvieira@gmail.com>
2016-10-10 21:51:23 -0300

    configura paths

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,10 @@
 app.config(['$routeProvider', function($routeProvider){
   $routeProvider
     .when('/', {
-      templateUrl : 'views/list.html',
+      templateUrl : 'app/views/list.html',
       controller : 'ListController'
     })
-    .when('/podcast/:podcastSlug', {
+    .when('data/podcast/:podcastSlug', {
       templateUrl : 'views/get.html',
       controller : 'GetController'
     })
