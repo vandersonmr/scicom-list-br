57ca3786a3ab8da3fbc13857fbed659ce9190947
ogilvieira <ogilvieira@gmail.com>
2016-10-11 00:49:54 -0300

    add service para feed

--- app/factory/feed.js
+++ app/factory/feed.js
@@ -1,16 +1,10 @@
 app.factory('feed', ['$http', function($http){
   return {
     list: function(url, callback){
-      $http({
-        method: 'GET',
-        url: url,
-        timeout: 10000,
-        params: {}
+      $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q='+encodeURIComponent(url),{
       }).success(function(data){
-        console.log(data);
         callback(data);
       }).error(function(){
-        console.warn("error feed");
         callback(false);
       });
     }
