36041a6c129c793272f355be8ce1d55b91cd2286
Gil Vieira <ogilvieira@gmail.com>
2016-10-10 21:44:03 -0300

    reorganiza arquitetura de pastas

--- js/factory/podcasts.js
+++ js/factory/podcasts.js
@@ -1,11 +1,11 @@
 app.factory('podcasts', function($http, Slug){
   return {
     list: function(callback){
-      $http.get('./podcasts.json?'+Date.now()).then(callback, callback(false));
+      $http.get('./data/podcasts.json?'+Date.now()).then(callback, callback(false));
     },
     get: function(slug, callback){
       var r;
-      $http.get('./podcasts.json?'+Date.now()).then(function(res){
+      $http.get('./data/podcasts.json?'+Date.now()).then(function(res){
         for(var i = 0; i < res.data.length; i++){
           if(slug == Slug.slugify(res.data[i].name)){
             r = res.data[i];
