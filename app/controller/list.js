f0f5672e6b899a4fcbaac2584fdd03c0675625cf
ogilvieira <ogilvieira@gmail.com>
2016-10-12 01:43:34 -0300

    melhorias na busca

--- app/controller/list.js
+++ app/controller/list.js
@@ -8,15 +8,18 @@ app.controller("ListController", ['$scope', 'podcasts', 'feed', 'ngMeta', functi
 
   (function(){
     var f = localStorage.getItem('searchform');
-    if(f){
-      $scope.searchform = JSON.parse(f);
-    }
+    if(f){ $scope.searchform = JSON.parse(f); }
   })();
 
   $scope.stripTags = function(text){
     var regex = /(<([^>]+)>)/ig,
         result = text.replace(regex, "");
         return result;
+  };
+
+  $scope.clearQuery = function(){
+    localStorage.removeItem('searchform');
+    $scope.searchform = {};
   };
 
   $scope.$watch("searchform", function(){
